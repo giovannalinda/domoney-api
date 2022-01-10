f0607ec144010364acaf481d9f7b8547fb7ea3d8
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-01-10 17:46:48 -0300

    Added eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express'
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.listen(PORT, () => {
-  console.log(`🔥 Server started at http://localhost:${PORT}`);
-});
+  console.log(`🔥 Server started at http://localhost:${PORT}`)
+})
