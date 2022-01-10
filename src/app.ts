cfd94e2d81766070bdc28e01a3875df4b8417683
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-01-10 17:52:38 -0300

    Installed cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 const PORT = process.env.PORT
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`🔥 Server started at http://localhost:${PORT}`)
