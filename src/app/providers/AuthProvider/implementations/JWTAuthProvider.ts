4813ce70c6d4a9b7b07de5019cec58c06440541e
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-01-14 12:51:58 -0300

    Added unit test in JWTAuthProvider

--- src/app/providers/AuthProvider/implementations/JWTAuthProvider.ts
+++ src/app/providers/AuthProvider/implementations/JWTAuthProvider.ts
@@ -2,11 +2,11 @@ import { sign, verify } from 'jsonwebtoken'
 
 import { AppError } from '@/lib'
 import { AuthProvider, TokenPayload } from '@/app/providers/AuthProvider/types'
-
-const JWT_SECRET = process.env.JWT_SECRET
 class JWTAuthProvider implements AuthProvider {
+  private JWT_SECRET = process.env.JWT_SECRET
+
   public generateToken(payload: string) {
-    return sign({ id: payload }, JWT_SECRET, {
+    return sign({ id: payload }, this.JWT_SECRET, {
       subject: payload,
       expiresIn: '1d',
     })
@@ -14,7 +14,7 @@ class JWTAuthProvider implements AuthProvider {
 
   public verifyToken(token: string): string {
     try {
-      const { sub } = verify(token, JWT_SECRET) as TokenPayload
+      const { sub } = verify(token, this.JWT_SECRET) as TokenPayload
 
       return sub
     } catch {
